635dea779935dc3a50b636f207a0ab5a8ad6e1d6
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-12-11 17:57:37 +0100

    Add files via upload

---
+++ ZadatciZaVjezbu05IvanMocilac/ZadatciZaVjezbu05IvanMocilac/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatciZaVjezbu05IvanMocilac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            int y = 1;
+            for (int i = 0; i < 100; i++)
+            {
+                arr[i] = y;
+                y = y + 2;
+            }
+            Search search = new Search();
+            Console.WriteLine(search.BinarySearch(arr, 75));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ZadatciZaVjezbu05IvanMocilac/ZadatciZaVjezbu05IvanMocilac/Search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatciZaVjezbu05IvanMocilac
+{
+    class Search
+    {
+        public int BinarySearch(int[] arr, int x)
+        {
+            int nisko = 0;
+            int visoko = arr.Length - 1;
+            int srednje;
+
+            while(nisko <= visoko)
+            {
+                srednje = (nisko + visoko) / 2;
+                if (x > arr[srednje])
+                {
+                    nisko = srednje + 1;
+                }
+                else if (x < arr[srednje])
+                {
+                    visoko = srednje - 1;
+                }
+                else
+                {
+                    return srednje;
+                }
+            }
+            return -1;
+        }
+    }
+}
